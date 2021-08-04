07d1db51a3f3e20e200f6c8a5397624f58c9b7af
mjos7 <markjnjoseph@gmail.com>
2021-08-04 09:08:29 -0700

    complete file structure

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 // TODO: Include packages needed for this application
 
+const profileDataArgs = process.argv.slice(2);
+
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
