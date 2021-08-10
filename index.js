b18e69f30fb01e1010ca72ebc6b329773d0bc5e4
mjos7 <83667780+mjos7@users.noreply.github.com>
2021-08-09 17:51:41 -0700

    write read me complete

--- index.js
+++ index.js
@@ -1,160 +1,170 @@
 'use strict';
 
+const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./src/page-template');
-const { writeFile, copyFile } = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-const questions = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is the name of your project? (required)',
-      validate: projectName => {
-        if (projectName) {
-          return true;
-        } else {
-          console.log('Please enter your Project Name');
-          return false;
-        }
-      },
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Provide a brief description of the project (required)',
-      validate: projDescription => {
-        if (projDescription) {
-          return true;
-        } else {
-          console.log('Please enter a Project Description');
-          return false;
-        }
-      },
-    },
-    {
-      type: 'input',
-      name: 'app-link',
-      message: 'Enter the link to your deployed application. (optional)',
-    },
-    {
-      type: 'input',
-      name: 'mockup-link',
-      message: 'Enter the link to a screenshot of your application. (optional)',
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'What are the steps required to install your project?',
-      validate: installation => {
-        if (installation) {
-          return true;
-        } else {
-          console.log('Please enter instructions for installing your project');
-          return false;
-        }
-      },
-    },
-
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Provide instructions and examples for use.',
-      validate: usage => {
-        if (usage) {
-          return true;
-        } else {
-          console.log('Please provide instructions and examples for use');
-          return false;
-        }
-      },
-    },
-
-    {
-      type: 'list',
-      name: 'license',
-      message: 'Please select your license from the options below.',
-      choices: [
-        'License preferred by community',
-        'MIT License',
-        'GNU GPLv3',
-        'GNU AGPLv3',
-        'Mozilla Public License 2.0',
-        'Apache License 2.0',
-        'Boost Software License 1.0',
-        'The Unlicense',
-      ],
-      validate: license => {
-        if (license) {
-          return true;
-        } else {
-          console.log('Please select a license from the options given');
-          return false;
-        }
-      },
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message:
-        'Provide test instructions for your application and provide examples on how to run them. (optional)',
-    },
-    {
-      type: 'input',
-      name: 'queastions',
-      message: 'Provide instructions and examples for use.',
-      validate: usage => {
-        if (usage) {
-          return true;
-        } else {
-          console.log('Please provide instructions and examples for use');
-          return false;
-        }
-      },
-    },
-
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
-      validate: githubUsername => {
-        if (githubUsername) {
-          return true;
-        } else {
-          console.log('Please enter your GitHub Username');
-          return false;
-        }
-      },
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'What is your Email Address?',
-      validate: email => {
-        if (githubUrl) {
-          return true;
-        } else {
-          console.log(
-            'Please enter your Email so others can contact you if they have questions'
-          );
-          return false;
-        }
-      },
-    },
-  ]);
+const promptUser = () => {
+  return inquirer.prompt(questions);
 };
 
-// fs.writeFile('./README.md', generatePage(name, github), err => {
-//   if (err) throw new Error(err);
+// STARTER CODE START //
 
-//   console.log('ReadMe complete! Check out readme.md to see the output!');
-// });
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'projectName',
+    message: 'What is the name of your project? (required)',
+    validate: projectName => {
+      if (projectName) {
+        return true;
+      } else {
+        console.log('Please enter your Project Name');
+        return false;
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a brief description of the project (required)',
+    validate: projDescription => {
+      if (projDescription) {
+        return true;
+      } else {
+        console.log('Please enter a Project Description');
+        return false;
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'appLink',
+    message: 'Enter the link to your deployed application. (optional)',
+  },
+  {
+    type: 'input',
+    name: 'mockupLink',
+    message: 'Enter the link to a screenshot of your application. (optional)',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the steps required to install your project?',
+    validate: installation => {
+      if (installation) {
+        return true;
+      } else {
+        console.log('Please enter instructions for installing your project');
+        return false;
+      }
+    },
+  },
+
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide instructions and examples for use.',
+    validate: usage => {
+      if (usage) {
+        return true;
+      } else {
+        console.log('Please provide instructions and examples for use');
+        return false;
+      }
+    },
+  },
+
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Please select your license from the options below.',
+    choices: [
+      'License preferred by community',
+      'MIT License',
+      'GNU GPLv3',
+      'GNU AGPLv3',
+      'Mozilla Public License 2.0',
+      'Apache License 2.0',
+      'Boost Software License 1.0',
+      'The Unlicense',
+      'No License',
+    ],
+    validate: license => {
+      if (license) {
+        return true;
+      } else {
+        console.log('Please select a license from the options given');
+        return false;
+      }
+    },
+  },
+
+  {
+    type: 'input',
+    name: 'contributions',
+    message:
+      'Please enter guidelines for how others can contibute to your project. (optional)',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message:
+      'Provide test instructions for your application and provide examples on how to run them. (optional)',
+  },
+
+  {
+    type: 'input',
+    name: 'githubUsername',
+    message: 'Enter your GitHub Username',
+    validate: githubUsername => {
+      if (githubUsername) {
+        return true;
+      } else {
+        console.log('Please enter your GitHub Username');
+        return false;
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your Email Address?',
+    validate: email => {
+      if (email) {
+        return true;
+      } else {
+        console.log(
+          'Please enter your Email so others can contact you if they have questions'
+        );
+        return false;
+      }
+    },
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('src/README.md', data, err => {
+      if (err) {
+        console.log('There is an error');
+        return;
+      }
+      resolve({
+        message: 'Your REAdME.md file has been created in the source folder',
+      });
+    });
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  promptUser().then(data => writeToFile(generateMarkdown(data)));
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,128 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'No License') {
+    return `https://img.shields.io/badge/license-${license}-blue.svg`;
+  }
+  return ``;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'No License') {
+    return `* [License](#license)`;
+  }
+  return ``;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
-}
+// STARTER CODE END //
+
+const generateMarkdown = data => {
+  return `
+    # ${projectName}
+  
+  
+    ## Description   
+    
+    ![Screenshot of Mockup](${mockupLink})
+  
+    ${description}
+  
+    ### Deployed Application
+    ${appLink}
+  
+  
+    ## Table of Contents   
+  
+    * [Installation](#installation)
+    * [Usage](#usage)
+    ${renderLicenseLink(data.license)}
+    * [Contributions](#contributions)
+    * [Tests](#tests)
+    * [Questions](#questions)
+  
+  
+    ## Installation
+  
+    ${installation}
+  
+  
+    ## Usage
+  
+    ${usage}
+  
+  
+    ## Credits
+  
+    ${credits}
+  
+  
+    ## License
+    
+    ${license}
+  
+  
+    ## Badges
+  
+    ${licenseBadge}
+    
+  
+    ## Features
+    ${features}
+  
+    ## Contributions
+    ${contributions}
+  
+  
+    ## Tests
+    ${tests}
+  
+    ## Questions
+  
+    For any questions, please don't hesitate to reach out to ${githubUsername} at ${email}
+  
+      `;
+};
 
 module.exports = generateMarkdown;
+
+// const generateMarkdown = fileContent => {
+//   return new Promise((resolve, reject) => {
+//     fs.writeFile('./dist/index.html', fileContent, err => {
+//       // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+//       if (err) {
+//         reject(err);
+//         // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+//         return;
+//       }
+
+//       // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+//       resolve({
+//         ok: true,
+//         message: 'File created!',
+//       });
+//     });
+//   });
+// };
+
+// const copyFile = () => {
+//   return new Promise((resolve, reject) => {
+//     fs.copyFile('./app.js', fileContent, err => {
+//       if (err) {
+//         reject(err);
+//         return;
+//       }
+//       resolve({
+//         ok: true,
+//         message: 'File copied successfully',
+//       });
+//     });
+//   });
+// };
